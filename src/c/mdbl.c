0594613cc9729541f3437f7fe8b2c78b4a30fefd
Cory Hughart <cory@coryhughart.com>
2026-04-28 23:10:00 -0500

    extra instrumentation for dev builds

--- src/c/mdbl.c
+++ src/c/mdbl.c
@@ -4,7 +4,15 @@ int main(void) {
 	Window *w = window_create();
 	window_stack_push(w, true);
 
+#ifdef ALLOY_INSTRUMENTATION
+	ModdableCreationRecord creation = {
+		.recordSize = sizeof(ModdableCreationRecord),
+		.flags = kModdableCreationFlagLogInstrumentation,
+	};
+	moddable_createMachine(&creation);
+#else
 	moddable_createMachine(NULL);
+#endif
 
 	window_destroy(w);
 }
